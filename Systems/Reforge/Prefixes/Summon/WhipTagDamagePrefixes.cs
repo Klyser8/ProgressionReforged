2e4ba46fb22d25902d7d320b800e13b45691c7bd
Klyser8 <35845031+Klyser8@users.noreply.github.com>
2025-07-08 15:24:12 +0200

    Tweaked whip tag damage prefix numbers

--- Systems/Reforge/Prefixes/Summon/WhipTagDamagePrefixes.cs
+++ Systems/Reforge/Prefixes/Summon/WhipTagDamagePrefixes.cs
@@ -5,34 +5,34 @@ namespace ProgressionReforged.Systems.Reforge.Prefixes.Summon;
 public class UnmarkedPrefix() : SimpleWhipTagDamagePrefix(
     -1,
     "whipTagDamage",
-    whipTagDamageMult: 0.90f,
+    whipTagDamageMult: 0.75f, // -25% whip tag damage
     next: () => ModContent.GetInstance<FlaggedPrefix>().Type,
     previous: () => -1);
 
 public class FlaggedPrefix() : SimpleWhipTagDamagePrefix(
     0,
     "whipTagDamage",
-    whipTagDamageMult: 1.05f,
+    whipTagDamageMult: 1.10f, // +10% whip tag damage
     next: () => ModContent.GetInstance<BrandingPrefix>().Type,
     previous: () => ModContent.GetInstance<UnmarkedPrefix>().Type);
 
 public class BrandingPrefix() : SimpleWhipTagDamagePrefix(
     1,
     "whipTagDamage",
-    whipTagDamageMult: 1.15f,
+    whipTagDamageMult: 1.25f, // +25% whip tag damage
     next: () => ModContent.GetInstance<DesignatedPrefix>().Type,
     previous: () => ModContent.GetInstance<FlaggedPrefix>().Type);
 
 public class DesignatedPrefix() : SimpleWhipTagDamagePrefix(
     2,
     "whipTagDamage",
-    whipTagDamageMult: 1.30f,
+    whipTagDamageMult: 1.45f, // +45% whip tag damage
     next: () => ModContent.GetInstance<CondemningPrefix>().Type,
     previous: () => ModContent.GetInstance<BrandingPrefix>().Type);
 
 public class CondemningPrefix() : SimpleWhipTagDamagePrefix(
     3,
     "whipTagDamage",
-    whipTagDamageMult: 1.50f,
+    whipTagDamageMult: 1.70f, // +70% whip tag damage
     next: () => -1,
     previous: () => ModContent.GetInstance<DesignatedPrefix>().Type);
