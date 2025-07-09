e16442df028e297ac79fe5c20a808ab6544f0fd9
Klyser8 <35845031+Klyser8@users.noreply.github.com>
2025-07-09 11:12:42 +0200

    Fixed jump height prefix stats

--- Systems/Reforge/Prefixes/Accessories/JumpHeightPrefixes.cs
+++ Systems/Reforge/Prefixes/Accessories/JumpHeightPrefixes.cs
@@ -5,28 +5,28 @@ namespace ProgressionReforged.Systems.Reforge.Prefixes.Accessories;
 public class WeightedPrefixes() : SimpleAccessoryPrefixes(
     -1,
     "acc.jumpHeight",
-    jumpHeightMult: 0.90f, // -10% jump height
+    jumpHeightMult: 0.88f, // -12% jump height
     next: () => ModContent.GetInstance<BuoyantPrefixes>().Type,
     previous: () => -1);
     
 public class BuoyantPrefixes() : SimpleAccessoryPrefixes(
     0,
     "acc.jumpHeight",
-    jumpHeightMult: 1.10f, // +6% jump height
+    jumpHeightMult: 1.06f, // +6% jump height
     next: () => ModContent.GetInstance<SpringyPrefixes>().Type,
     previous: () => ModContent.GetInstance<WeightedPrefixes>().Type);
     
 public class SpringyPrefixes() : SimpleAccessoryPrefixes(
     1,
     "acc.jumpHeight",
-    jumpHeightMult: 1.20f, // +12% jump height
+    jumpHeightMult: 1.12f, // +12% jump height
     next: () => ModContent.GetInstance<LeapingPrefixes>().Type,
     previous: () => ModContent.GetInstance<BuoyantPrefixes>().Type);
     
 public class LeapingPrefixes() : SimpleAccessoryPrefixes(
     2,
     "acc.jumpHeight",
-    jumpHeightMult: 1.30f, // +18% jump height
+    jumpHeightMult: 1.18f, // +18% jump height
     next: () => ModContent.GetInstance<AirbornPrefixes>().Type,
     previous: () => ModContent.GetInstance<SpringyPrefixes>().Type);
     
